ccdd9c91b7820d6485626ec1fccb4f49bca11537
livia <livia@earthwave.co.uk>
2022-07-28 19:00:44 +0100

    data catalogue changes and unit test fixes

--- glambie/data/data_catalogue.py
+++ glambie/data/data_catalogue.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
-
-from typing import Optional
-from webbrowser import get
 import pandas as pd
 import json
 
 from glambie.const.data_groups import GLAMBIE_DATA_GROUPS
-from glambie.const.regions import regions
+from glambie.const.regions import REGIONS, RGIRegion
 from glambie.data.timeseries import Timeseries
 import os
 
@@ -14,16 +11,17 @@ import os
 class DataCatalogue():
     """Class containing a catalogue of datasets
 
-    This only contains metedata - all actual data loaded by client
+    This only contains metadata - all actual data loaded by client
     """
-    def __init__(self, base_path: str, datasets: dict[str, Timeseries]):
-        self.base_path = base_path
+
+    def __init__(self, base_path: str, datasets: list[Timeseries]):
+        self._base_path = base_path
         self._datasets = datasets
 
     @staticmethod
     def from_json_file(metadata_file_path: str) -> DataCatalogue:
         with open(metadata_file_path) as json_file:
-            DataCatalogue.from_dict(json.load(json_file))
+            return DataCatalogue.from_dict(json.load(json_file))
 
     @staticmethod
     def from_dict(meta_data_dict: dict) -> DataCatalogue:
@@ -32,35 +30,31 @@ class DataCatalogue():
         datasets = []
         for ds_dict in datasets_dict:
             fp = os.path.join(basepath, ds_dict['filename'])
-            region = regions[ds_dict['region']]
+            region = REGIONS[ds_dict['region']]
             data_group = GLAMBIE_DATA_GROUPS[ds_dict['data_group']]
             user_group = ds_dict['user_group']
-            dataset = Timeseries(data_filepath=fp, region=region, data_group=data_group, user_group=user_group)
-            datasets.append(dataset)
+            datasets.append(Timeseries(data_filepath=fp, region=region, data_group=data_group, user_group=user_group))
 
         return DataCatalogue(basepath, datasets)
 
     @property
-    def datasets(self):
+    def datasets(self) -> list[Timeseries]:
         return self._datasets
 
     @property
-    def regions(self):
-        return self._regions
+    def regions(self) -> list[RGIRegion]:
+        return list({s.region for s in self._datasets})  # get as a set, so only unique values
 
     @property
-    def basepath(self):
-        return self._basepath
+    def base_path(self) -> str:
+        return self._base_path
 
-    def as_dataframe(self):
+    def as_dataframe(self) -> pd.DataFrame:
         metadata_list = [ds.metadata_as_dataframe() for ds in self._datasets]
         return pd.concat(metadata_list)
 
-    def get_regions(self) -> list:
-        return list({s.region for s in self._datasets})  # get as a set, so only unique values
-
-    def get_filtered_datasets(self, region_name: Optional[str] = None, data_group: Optional[str] = None,
-                              user_group: Optional[str] = None):
+    def get_filtered_catalogue(self, region_name: str = None, data_group: str = None,
+                               user_group: str = None):
         datasets = self._datasets
         if region_name is not None:  # filter by region
             datasets = [s for s in datasets if s.region.name.lower() == region_name.lower()]
--- tests/data/test_data_catalogue.py
+++ tests/data/test_data_catalogue.py
@@ -8,25 +8,25 @@ from glambie.data.data_catalogue import DataCatalogue
 @pytest.fixture()
 def example_catalogue():
     return DataCatalogue.from_dict({"basepath": ["tests", "test_data", "datastore"],
-                          "datasets": [
-                              {
-                                  "filename": "iceland_altimetry_sharks.csv",
-                                  "region": "iceland",
-                                  "user_group": "sharks",
-                                  "data_group": "altimetry"
-                              },
-                              {
-                                  "filename": "svalbard_altimetry_sharks.csv",
-                                  "region": "svalbard",
-                                  "user_group": "sharks",
-                                  "data_group": "altimetry"
-                              },
-                              {
-                                  "filename": "svalbard_altimetry_lions.csv",
-                                  "region": "svalbard",
-                                  "user_group": "lions",
-                                  "data_group": "gravimetry"
-                              }]})
+                                    "datasets": [
+        {
+            "filename": "iceland_altimetry_sharks.csv",
+            "region": "iceland",
+            "user_group": "sharks",
+            "data_group": "altimetry"
+        },
+        {
+            "filename": "svalbard_altimetry_sharks.csv",
+            "region": "svalbard",
+            "user_group": "sharks",
+            "data_group": "altimetry"
+        },
+        {
+            "filename": "svalbard_altimetry_lions.csv",
+            "region": "svalbard",
+            "user_group": "lions",
+            "data_group": "gravimetry"
+        }]})
 
 
 def test_data_catalogue_can_be_initiated(example_catalogue):
@@ -45,27 +45,27 @@ def test_data_catalogue_datasets_correctly_ingested(example_catalogue):
     assert example_catalogue.datasets[2].data_group.name == 'gravimetry'
 
 
-def test_get_filtered_datasets_by_region(example_catalogue):
-    assert len(example_catalogue.get_filtered_datasets(region_name='svalbard')) == 2
+def test_get_filtered_catalogue_by_region(example_catalogue):
+    assert len(example_catalogue.get_filtered_catalogue(region_name='svalbard')) == 2
 
 
-def test_get_filtered_datasets_by_data_group(example_catalogue):
-    assert len(example_catalogue.get_filtered_datasets(data_group='altimetry')) == 2
-    assert len(example_catalogue.get_filtered_datasets(data_group='gravimetry')) == 1
-    assert example_catalogue.get_filtered_datasets(data_group='gravimetry')[0].data_group.name == 'gravimetry'
+def test_get_filtered_catalogue_by_data_group(example_catalogue):
+    assert len(example_catalogue.get_filtered_catalogue(data_group='altimetry')) == 2
+    assert len(example_catalogue.get_filtered_catalogue(data_group='gravimetry')) == 1
+    assert example_catalogue.get_filtered_catalogue(data_group='gravimetry').datasets[0].data_group.name == 'gravimetry'
 
 
-def test_get_filtered_datasets_by_region_and_data_group(example_catalogue):
-    assert len(example_catalogue.get_filtered_datasets(region_name='svalbard',
+def test_get_filtered_catalogue_by_region_and_data_group(example_catalogue):
+    assert len(example_catalogue.get_filtered_catalogue(region_name='svalbard',
                data_group='altimetry')) == 1
-    assert len(example_catalogue.get_filtered_datasets(region_name='svalbard',
+    assert len(example_catalogue.get_filtered_catalogue(region_name='svalbard',
                data_group='altimetry')) == 1
-    assert example_catalogue.get_filtered_datasets(data_group='gravimetry')[0].data_group.name == 'gravimetry'
+    assert example_catalogue.get_filtered_catalogue(data_group='gravimetry').datasets[0].data_group.name == 'gravimetry'
 
 
-def test_get_filtered_datasets_by_user_group(example_catalogue):
-    assert len(example_catalogue.get_filtered_datasets(user_group='sharks')) == 2
-    assert len(example_catalogue.get_filtered_datasets(user_group='lions')) == 1
+def test_get_filtered_catalogue_by_user_group(example_catalogue):
+    assert len(example_catalogue.get_filtered_catalogue(user_group='sharks')) == 2
+    assert len(example_catalogue.get_filtered_catalogue(user_group='lions')) == 1
 
 
 def test_as_dataframe(example_catalogue):
@@ -74,5 +74,8 @@ def test_as_dataframe(example_catalogue):
 
 
 def test_data_catalogue_from_file():
-    catalogue = DataCatalogue.from_file(os.path.join('tests', 'test_data', 'datastore', 'meta.json'))
+    catalogue = DataCatalogue.from_json_file(os.path.join('tests', 'test_data', 'datastore', 'meta.json'))
     assert len(catalogue.datasets) == 4
+
+
+# @ TODO Sophie: write unit test for data_catalogue.regions

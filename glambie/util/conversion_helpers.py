7b60c51513feb60ecd7b2cf6e0e627410789165c
Sophie Dubber <sophie.dubber@earthwave.co.uk>
2022-08-24 15:49:19 +0100

    Linter wasn't working fixed now

--- glambie/util/conversion_helpers.py
+++ glambie/util/conversion_helpers.py
@@ -1,4 +1,4 @@
-def meters_to_gigatonnes(variable_in_m: float, rgi_area_km2: float, density_of_ice_in_Gt_per_m3: float = 850) -> float:
+def meters_to_gigatonnes(variable_in_m: float, rgi_area_km2: float, density_of_ice_in_gt_per_m3: float = 850) -> float:
     """
     Function to convert a measurement in meters to giga tonnes.
 
@@ -15,12 +15,12 @@ def meters_to_gigatonnes(variable_in_m: float, rgi_area_km2: float, density_of_i
     ----------
     Input variable converted into giga tonnes
     """
-    variable_in_gt = variable_in_m * density_of_ice_in_Gt_per_m3 * (rgi_area_km2 / 1e6)
+    variable_in_gt = variable_in_m * density_of_ice_in_gt_per_m3 * (rgi_area_km2 / 1e6)
     return variable_in_gt
 
 
-def meters_to_meters_water_equivalent(variable_in_m: float, density_of_water_in_Gt_per_m3: float = 997,
-                                      density_of_ice_in_Gt_per_m3: float = 850) -> float:
+def meters_to_meters_water_equivalent(variable_in_m: float, density_of_water_in_gt_per_m3: float = 997,
+                                      density_of_ice_in_gt_per_m3: float = 850) -> float:
     """
     Function to convert a measurement in meters to meters water equivalent. mwe will always be lower value than m,
     due to the difference in density of ice and water
@@ -39,5 +39,5 @@ def meters_to_meters_water_equivalent(variable_in_m: float, density_of_water_in_
     -------
     Input variable converted into meters water equivalent
     """
-    variable_in_mwe = (variable_in_m / density_of_water_in_Gt_per_m3) * density_of_ice_in_Gt_per_m3
+    variable_in_mwe = (variable_in_m / density_of_water_in_gt_per_m3) * density_of_ice_in_gt_per_m3
     return variable_in_mwe
--- tests/util/test_conversion_helpers.py
+++ tests/util/test_conversion_helpers.py
@@ -1,20 +1,21 @@
 from glambie.util.conversion_helpers import meters_to_gigatonnes, meters_to_meters_water_equivalent
 
+
 def test_meters_to_gigatonnes():
     test_variable_in_m = 20
     test_rgi_area_km2 = 1000
     assert meters_to_gigatonnes(test_variable_in_m, test_rgi_area_km2) == 17
     # if non-default ice density is used
-    test_density_of_ice_in_Gt_per_m3 = 800
-    assert meters_to_gigatonnes(test_variable_in_m, test_rgi_area_km2, test_density_of_ice_in_Gt_per_m3) == 16
-    
-    
+    test_density_of_ice_in_gt_per_m3 = 800
+    assert meters_to_gigatonnes(test_variable_in_m, test_rgi_area_km2, test_density_of_ice_in_gt_per_m3) == 16
+
+
 def test_meters_to_meters_water_equivalent():
     test_variable_in_m = 20
-    assert meters_to_meters_water_equivalent(test_variable_in_m) == (20/997)*850 #Is this the best way? round() instead?
+    assert meters_to_meters_water_equivalent(test_variable_in_m) == (20 / 997) * 850  # Is this ok? round() instead?
     # if non-default ice density is used
-    test_density_of_ice_in_Gt_per_m3 = 800
-    assert meters_to_meters_water_equivalent(test_variable_in_m, test_density_of_ice_in_Gt_per_m3) == (20/997)*800
+    test_density_of_ice_in_gt_per_m3 = 800
+    assert meters_to_meters_water_equivalent(test_variable_in_m, test_density_of_ice_in_gt_per_m3) == (20 / 997) * 800
     # if non-default water density is used
-    test_density_of_water_in_Gt_per_m3 = 950
-    assert meters_to_meters_water_equivalent(test_variable_in_m, test_density_of_water_in_Gt_per_m3) == (20/950)*850
+    test_density_of_water_in_gt_per_m3 = 950
+    assert meters_to_meters_water_equivalent(test_variable_in_m, test_density_of_water_in_gt_per_m3) == (20 / 950) * 850

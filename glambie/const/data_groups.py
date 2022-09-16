ec1ddbbde4929702364d7e87689819b6bbdef561
Sophie Dubber <sophie.dubber@earthwave.co.uk>
2022-09-16 12:52:20 +0100

    fixed typo

--- glambie/const/data_groups.py
+++ glambie/const/data_groups.py
@@ -11,6 +11,6 @@ GLAMBIE_DATA_GROUPS = {
     'altimetry': GlambieDataGroup(name='altimetry', long_name='Altimetry'),
     'demdiff': GlambieDataGroup(name='demdiff', long_name='DEM differencing'),
     'gravimetry': GlambieDataGroup(name='gravimetry', long_name='Gravimetry'),
-    'glaciological': GlambieDataGroup(name='glaciological', lond_name='Glaciological'),
+    'glaciological': GlambieDataGroup(name='glaciological', long_name='Glaciological'),
     'combined': GlambieDataGroup(name='combined', long_name='Combination of methods')
 }

4cbe39b1c0017f00e5eda84018901db5d157a3c2
idussa <ines.dussaillant@geo.uzh.ch>
2022-10-11 14:19:50 +0200

    correct spelling

--- glambie/entrypoint.py
+++ glambie/entrypoint.py
@@ -1,5 +1,5 @@
 """
-The single comand line entrypoint for the package.
+The single command line entry point for the package.
 Currently a placeholder
 """
 import argparse
@@ -8,8 +8,8 @@ import argparse
 def main():
     # Build the argument parser
     parser = argparse.ArgumentParser(
-        prog='glambie', description='Glambie entry point.')
-    parser.add_argument('-config', dest='config', type=str, help='The first integer')
+        prog='glambie', description='GlaMBIE entry point.')
+    parser.add_argument('-config', dest='config', type=str, help='The config path')
 
     # parse the input arguments
     args = parser.parse_args()

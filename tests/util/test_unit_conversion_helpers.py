9dd118340ef84491cac68dcd7d098a492d174def
Sophie Dubber <sophie.dubber@earthwave.co.uk>
2022-10-14 16:14:42 +0100

    Changed names in testing

--- tests/util/test_unit_conversion_helpers.py
+++ tests/util/test_unit_conversion_helpers.py
@@ -1,9 +1,9 @@
-from glambie.util.unit_conversion_helpers import meter2gigatonne
-from glambie.util.unit_conversion_helpers import gigatonne2meter
-from glambie.util.unit_conversion_helpers import meter2mwe
-from glambie.util.unit_conversion_helpers import mwe2meter
-from glambie.util.unit_conversion_helpers import gigatonne2slr
-from glambie.util.unit_conversion_helpers import gigatonne2mwe
+from glambie.util.unit_conversion_helpers import _convert_meters_to_gigatonnes
+from glambie.util.unit_conversion_helpers import _convert_gigatonnes_to_meters
+from glambie.util.unit_conversion_helpers import _convert_meters_to_meters_water_equivalent
+from glambie.util.unit_conversion_helpers import _convert_meters_water_equivalent_to_meters
+from glambie.util.unit_conversion_helpers import _convert_gigatonnes_to_sea_level_rise
+from glambie.util.unit_conversion_helpers import _convert_gigatonnes_to_meters_water_equivalent
 
 from glambie.util.unit_conversion_helpers import meters_to_gigatonnes
 from glambie.util.unit_conversion_helpers import gigatonnes_to_meters
@@ -16,62 +16,64 @@ from glambie.util.unit_conversion_helpers import gigatonnes_to_meters_water_equi
 def test_meters2gigatonnes():
     test_variable_in_m = 20
     test_rgi_area_km2 = 1000
-    assert meter2gigatonne(20, 1000) == 17
+    assert _convert_meters_to_gigatonnes(20, 1000) == 17
     # if non-default ice density is used
     test_density_of_ice_in_gt_per_m3 = 800
-    assert meter2gigatonne(test_variable_in_m, test_rgi_area_km2, test_density_of_ice_in_gt_per_m3) == 16
+    assert _convert_meters_to_gigatonnes(test_variable_in_m, test_rgi_area_km2, test_density_of_ice_in_gt_per_m3) == 16
 
 
 def test_gigatonnes2meters():
     test_variable_in_gt = 17
     test_rgi_area_km2 = 1000
-    assert gigatonne2meter(test_variable_in_gt, test_rgi_area_km2) == 20
+    assert _convert_gigatonnes_to_meters(test_variable_in_gt, test_rgi_area_km2) == 20
     # if non-default ice density is used
     test_density_of_ice_in_gt_per_m3 = 800
-    assert gigatonne2meter(test_variable_in_gt, test_rgi_area_km2, test_density_of_ice_in_gt_per_m3) == 21.25
+    assert _convert_gigatonnes_to_meters(test_variable_in_gt, test_rgi_area_km2,
+                                         test_density_of_ice_in_gt_per_m3) == 21.25
 
 
 def test_meters2mwe():
     test_variable_in_m = 20
-    assert meter2mwe(test_variable_in_m) == (20 / 997) * 850
+    assert _convert_meters_to_meters_water_equivalent(test_variable_in_m) == (20 / 997) * 850
     # if non-default ice density is used
-    test_density_of_ice_in_gt_per_m3 = 800
-    assert meter2mwe(test_variable_in_m,
-                     density_of_ice_in_gt_per_m3=test_density_of_ice_in_gt_per_m3) == (20 / 997) * 800
+    test_density_of_ice = 800
+    assert _convert_meters_to_meters_water_equivalent(test_variable_in_m,
+                                                      density_of_ice=test_density_of_ice) == (20 / 997) * 800
     # if non-default water density is used
-    test_density_of_water_in_gt_per_m3 = 950
-    assert meter2mwe(test_variable_in_m,
-                     density_of_water_in_gt_per_m3=test_density_of_water_in_gt_per_m3) == (20 / 950) * 850
+    test_density_of_water = 950
+    assert _convert_meters_to_meters_water_equivalent(test_variable_in_m,
+                                                      density_of_water=test_density_of_water) == (20 / 950) * 850
 
 
 def test_mwe2meters():
     test_variable_in_mwe = 20
-    assert mwe2meter(test_variable_in_mwe) == (20 * 997) / 850
+    assert _convert_meters_water_equivalent_to_meters(test_variable_in_mwe) == (20 * 997) / 850
     # if non-default ice density is used
-    test_density_of_ice_in_gt_per_m3 = 800
-    assert mwe2meter(test_variable_in_mwe,
-                     density_of_ice_in_gt_per_m3=test_density_of_ice_in_gt_per_m3) == (20 * 997) / 800
+    test_density_of_ice = 800
+    assert _convert_meters_water_equivalent_to_meters(test_variable_in_mwe,
+                                                      density_of_ice=test_density_of_ice) == (20 * 997) / 800
     # if non-default water density is used
-    test_density_of_water_in_gt_per_m3 = 950
-    assert mwe2meter(test_variable_in_mwe,
-                     density_of_water_in_gt_per_m3=test_density_of_water_in_gt_per_m3) == (20 * 950) / 850
+    test_density_of_water = 950
+    assert _convert_meters_water_equivalent_to_meters(test_variable_in_mwe,
+                                                      density_of_water=test_density_of_water) == (20 * 950) / 850
 
 
 def test_gigatonnes2mwe():
     test_variable_in_gt = 50
-    test_rgi_area_km2 = 1000
-    assert gigatonne2mwe(test_variable_in_gt, test_rgi_area_km2) == (1e6 * 50) / (1000 * 997)
-    test_density_of_water_in_gt_per_m3 = 950
-    assert gigatonne2mwe(test_variable_in_gt, test_rgi_area_km2,
-                         density_of_water_in_gt_per_m3=test_density_of_water_in_gt_per_m3) == (1e6 * 50) / (1000 * 950)
+    test_area = 1000
+    assert _convert_gigatonnes_to_meters_water_equivalent(test_variable_in_gt, test_area) == (1e6 * 50) / (1000 * 997)
+    test_density_of_water = 950
+    assert _convert_gigatonnes_to_meters_water_equivalent(
+        test_variable_in_gt, test_area, density_of_water=test_density_of_water) == (1e6 * 50) / (1000 * 950)
 
 
 def test_gigatonnes2slr():
     test_variable_in_gt = 50
-    assert gigatonne2slr(test_variable_in_gt) == abs(50 / 3.625e8) * 1e6
+    assert _convert_gigatonnes_to_sea_level_rise(test_variable_in_gt) == abs(50 / 3.625e8) * 1e6
     # if non-default ocean area is used
     test_ocean_area = 3.8e8
-    assert gigatonne2slr(test_variable_in_gt, ocean_area=test_ocean_area) == abs(50 / 3.8e8) * 1e6
+    assert _convert_gigatonnes_to_sea_level_rise(test_variable_in_gt,
+                                                 ocean_area=test_ocean_area) == abs(50 / 3.8e8) * 1e6
 
 
 def test_meters_to_gigatonnes():

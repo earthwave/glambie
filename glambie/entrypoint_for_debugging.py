816d33a04327fe436060a773508284b99038ca56
livia <livia@earthwave.co.uk>
2023-07-21 10:59:36 +0100

    rename config folder

---
+++ glambie/entrypoint_for_debugging.py
@@ -0,0 +1,21 @@
+"""
+Just a file for debudding the entrypoint"""
+from glambie.processing.main import run_glambie_assessment
+from glambie.config.config_classes import GlambieRunConfig
+from glambie.monitoring.logging import setup_logging
+from glambie.processing.path_handling import get_output_path_handler_with_timestamped_subfolder
+
+if __name__ == '__main__':
+    # get config object
+    config_path = "main_study_configs/0_parent_config.yaml"
+    config_object = GlambieRunConfig.from_yaml(config_path)
+    log_level = "DEBUG"
+    # set up logging
+    setup_logging(log_level=log_level)
+    glambie_run_config = GlambieRunConfig.from_yaml(config_path)
+
+    # get config object
+    output_path_handler = get_output_path_handler_with_timestamped_subfolder(glambie_run_config.result_base_path)
+
+    # run assessment
+    run_glambie_assessment(glambie_run_config=glambie_run_config, output_path_handler=output_path_handler)

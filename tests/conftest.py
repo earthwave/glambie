d38a2115e6c603b88168c3fa2d708897895d1580
Sophie Dubber <sophie.dubber@earthwave.co.uk>
2023-07-28 16:45:06 +0100

    Change to test from PR comments

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import pytest
-
-
-@pytest.fixture()
-def test_inputs_path():
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_data')
--- tests/data/test_read_and_edit_bucket_csv_files.py
+++ tests/data/test_read_and_edit_bucket_csv_files.py
@@ -2,12 +2,17 @@ import os
 import pytest
 import numpy as np
 import pandas as pd
-from glambie.data.read_and_edit_bucket_csv_files import download_csvs_from_bucket, check_glambie_submission_for_errors
+from glambie.data.submission_cleaning_script import download_csvs_from_bucket, check_glambie_submission_for_errors
+
+
+@pytest.fixture()
+def test_inputs_path():
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_data')
 
 
 @pytest.fixture()
 def example_file_check_dataframe():
-    return pd.DataFrame.from_dict({'local_filepath': ['dummy', 'dummy'],
+    return pd.DataFrame.from_dict({'local_filepath': 'dummy',
                                    'date_check_satisfied': np.nan,
                                    'nodata_check_satisfied': np.nan})
 
@@ -21,24 +26,30 @@ def test_download_csvs_from_bucket(tmp_path):
     assert len(downloaded_files) == 2
 
 
-def test_check_glambie_submission_for_errors(test_inputs_path, example_file_check_dataframe):
+def test_check_glambie_submission_for_errors_date_error(test_inputs_path, example_file_check_dataframe):
 
-    test_csv_date_error = os.path.join(test_inputs_path, 'submissions', 'test_glambie_submission_date_error.csv')
-    test_csv_nodata_error = os.path.join(test_inputs_path, 'submissions', 'test_glambie_submission_nodata_error.csv')
-    example_file_check_dataframe['local_filepath'] = [test_csv_date_error, test_csv_nodata_error]
+    test_csv_with_date_error = os.path.join(test_inputs_path, 'submissions', 'test_glambie_submission_date_error.csv')
+    example_file_check_dataframe['local_filepath'] = test_csv_with_date_error
 
-    example_file_check_dataframe = check_glambie_submission_for_errors(test_csv_date_error,
+    example_file_check_dataframe = check_glambie_submission_for_errors(test_csv_with_date_error,
                                                                        example_file_check_dataframe)
 
     assert not example_file_check_dataframe.loc[example_file_check_dataframe.local_filepath.__eq__(
-        test_csv_date_error), 'date_check_satisfied'].values[0]
+        test_csv_with_date_error), 'date_check_satisfied'].values[0]
     assert example_file_check_dataframe.loc[example_file_check_dataframe.local_filepath.__eq__(
-        test_csv_date_error), 'nodata_check_satisfied'].values[0]
+        test_csv_with_date_error), 'nodata_check_satisfied'].values[0]
 
-    example_file_check_dataframe = check_glambie_submission_for_errors(test_csv_nodata_error,
+
+def test_check_glambie_submission_for_errors_nodata_error(test_inputs_path, example_file_check_dataframe):
+
+    test_csv_with_nodata_error = os.path.join(
+        test_inputs_path, 'submissions', 'test_glambie_submission_nodata_error.csv')
+    example_file_check_dataframe['local_filepath'] = test_csv_with_nodata_error
+
+    example_file_check_dataframe = check_glambie_submission_for_errors(test_csv_with_nodata_error,
                                                                        example_file_check_dataframe)
 
     assert example_file_check_dataframe.loc[example_file_check_dataframe.local_filepath.__eq__(
-        test_csv_nodata_error), 'date_check_satisfied'].values[0]
+        test_csv_with_nodata_error), 'date_check_satisfied'].values[0]
     assert not example_file_check_dataframe.loc[example_file_check_dataframe.local_filepath.__eq__(
-        test_csv_nodata_error), 'nodata_check_satisfied'].values[0]
+        test_csv_with_nodata_error), 'nodata_check_satisfied'].values[0]

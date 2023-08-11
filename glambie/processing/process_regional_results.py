6c5a344570a02eb4c2bf1897f77ac798ff120e8e
Alex Horton <62298621+alex-ew@users.noreply.github.com>
2023-08-11 10:05:31 -0700

    Minor docstring typo

--- glambie/processing/process_regional_results.py
+++ glambie/processing/process_regional_results.py
@@ -216,7 +216,7 @@ def _run_region_timeseries_one_source(
         if specified, the time series are filtered by the time window before the longterm trend is extracted,
         meaning that the resulting longterm trends are within the minimum and maximum of the time window.
         Note that existing longterm trends are removed if they are outside the time window.
-        the dates are expected in decimal years format (float), e.g. 2012.75
+        The dates are expected in decimal years format (float), e.g. 2012.75.
 
     Returns
     -------

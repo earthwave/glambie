ac0b8f3b99f50843b0ef7fbe4ebaf165b8211f4e
“Livia <livia@earthwave.co.uk>
2026-08-16 13:52:48 +0100

    lint fix

--- glambie/data/data_catalogue.py
+++ glambie/data/data_catalogue.py
@@ -549,7 +549,9 @@ class DataCatalogue:
             uncertainty_level=reference_dataset_for_metadata.uncertainty_level
         ), data_catalogue_out
 
-    def convert_all_datasets_to_uncertainty_level(self, target_uncertainty_level: int) -> DataCatalogue:
+    def convert_all_datasets_to_uncertainty_level(
+        self, target_uncertainty_level: int
+    ) -> DataCatalogue:
         """
         Converts the uncertainty values of all datasets in the catalogue to the target uncertainty level.
 
@@ -563,7 +565,10 @@ class DataCatalogue:
         DataCatalogue
             New data catalogue with all datasets converted to the target uncertainty level.
         """
-        datasets = [ds.convert_timeseries_uncertainty_level(target_uncertainty_level) for ds in self._datasets]
+        datasets = [
+            ds.convert_timeseries_uncertainty_level(target_uncertainty_level)
+            for ds in self._datasets
+        ]
         return DataCatalogue.from_list(datasets, base_path=self._base_path)
 
     def __len__(self) -> int:
